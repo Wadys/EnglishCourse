ca0da87e287063330d556d87c5111a7c24f9c570
Wady <wadyprograming@gmail.com>
2024-11-15 08:19:01 -0600

    Updated to include overtime payment

--- GrossPay.py
+++ GrossPay.py
@@ -1,6 +1,12 @@
 ##This program will promt the user for the number of hours worked and the hourly rate, and then calculate the gross pay.
 print("Welcome to gross pay calculator please provide the following data:")
-hours = input("How many hours have you worked? ")
+##Update program to pay any hours over 40 at 1.5 rate
+hours = float(input("How many hours have you worked? "))
 rate = input("What is your hourly rate? ")
-grossPay = round(float(hours)*float(rate),2)
+OTHours = 0
+if hours > 40:
+    OTHours = hours - 40
+    hours = 40   
+    print(f"over time is {OTHours}")
+grossPay = round((float(hours)*float(rate))+(float(OTHours)*float(rate)*1.5),2)
 print(f"Your gross pay is {grossPay}")

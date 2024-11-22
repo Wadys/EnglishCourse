5c7504e32d4548e084232d13699f5e79d6fc07b3
Wady <wadyprograming@gmail.com>
2024-11-22 07:59:29 -0600

    Updating GrossPay to include exceptions

--- GrossPay.py
+++ GrossPay.py
@@ -1,8 +1,20 @@
 ##This program will promt the user for the number of hours worked and the hourly rate, and then calculate the gross pay.
 print("Welcome to gross pay calculator please provide the following data:")
-##Update program to pay any hours over 40 at 1.5 rate
-hours = float(input("How many hours have you worked? "))
+#15/11/24# Update program to pay any hours over 40 at 1.5 rate.
+#17/11/24# UpDated program to include Try and Except so program can handle non-nimeric
+#input gracefully by printing an error message and exiting the program.
+hours = input("How many hours have you worked? ")
+try: #Updated to include TryCatch errors
+    hours = float(hours)
+except ValueError: 
+    print("Error, please enter numeric input for Hours value")
+    quit()
 rate = input("What is your hourly rate? ")
+try:
+    hours = float(hours)
+except ValueError:
+    print("Error, please enter anumeric input for Rate value")
+    quit()
 OTHours = 0
 if hours > 40:
     OTHours = hours - 40

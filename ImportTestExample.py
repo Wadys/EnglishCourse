e14c8a89f154bdee95d4effb273f8887d3e88622
Wady <wadyprograming@gmail.com>
2024-11-25 07:06:01 -0600

    Created new file ImportTestExample

---
+++ ImportTestExample.py
@@ -0,0 +1,8 @@
+##This is an example to show how Imports works
+import math #This is a math librery that comes in Python 
+import GrossPay #This is a personal library from previouse examples created by us
+GrossPay.grossPay #This is the imported varible from Gross Pay lib
+result = math.pi #Imported math varible
+print(result)
+result = math.sqrt(16) #Imported Square Root function
+print(result)

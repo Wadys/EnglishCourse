012a3ee175a6a1a9c2dfb0898c404fb9f01f1105
Wady <wadyprograming@gmail.com>
2024-12-02 10:27:57 -0600

    Updating password logic to receive list

--- PasswordController.py
+++ PasswordController.py
@@ -1,10 +1,17 @@
 # Create a function which takes string password as a parameter and checks the length 
 # of password. If the length longer than 8 character it returns True otherwise 
 # returns False.
+#02/12/24 Upate function to be called using a list of passwords
+import os
 def password_controller(p):
     """"string is verified if is over 8 char long""" #Tells what the function does
     if len(p) > 8:
         return True
     else:
         return False
+#Main Program
+os.system('cls')
 print(password_controller("custompassword"))
+password_list = ["qwer","123456","abdcefgh","098765432"]
+for pwd in password_list:
+    print(pwd,password_controller(pwd))

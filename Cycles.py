7776d4b224a1c01a45fa83250848bf45d2f0e59d
Wady <wadyprograming@gmail.com>
2024-12-01 20:39:23 -0600

    Created file to show exaples of how for cycles work

---
+++ Cycles.py
@@ -0,0 +1,12 @@
+vegetables = ["Carrot", "Brocoli", "Corn"]
+for vegetable in vegetables:
+    print(vegetable)
+    print(vegetable + " Pie")
+print(vegetables)
+
+nums = [50, 90, 10]
+total = 0
+for num in nums:
+    total += num
+print(nums)
+print(total)

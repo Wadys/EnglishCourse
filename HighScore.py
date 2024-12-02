0ccf77bbc20751d44de5be38df2dbcdc24f5c460
Wady <wadyprograming@gmail.com>
2024-12-01 20:55:07 -0600

    Created High Score logic

---
+++ HighScore.py
@@ -0,0 +1,19 @@
+# Highest Score
+# A List of scores of students are given, implement a program that calculates
+# the highest score from the given list.
+# Example:
+# student_scores = [80, 60, 50, 65, 75, 55]
+# The highest score in the class is: 80
+import os
+def high_score(list):
+    h_score = 0
+    for num in list:
+        if h_score < num:
+            h_score = num
+    return h_score
+
+
+
+os.system('cls')
+student_scores = [80, 60, 50, 90, 65, 75, 55, 100]
+print(f"The highst score in the class is : {high_score(student_scores)}")

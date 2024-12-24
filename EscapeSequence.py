8bf8fc68877afacd95fddb4fea0328bb2a27363d
Wady <wadyprograming@gmail.com>
2024-12-24 10:38:36 -0600

    Adding quotation logic and escape secuence

---
+++ EscapeSequence.py
@@ -0,0 +1,11 @@
+#triple quotes
+question1 = '''He said "What's your name?"'''
+print(question1)
+
+#escaping single quotes
+question2 = 'He said "What\'s your name?"'
+print(question2)
+
+#escaping single quotes
+question3 = "He said \"What's your name?\""
+print(question3)

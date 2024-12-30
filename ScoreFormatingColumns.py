b2308c63f67e2625d1f72ea3bc2847688b06aaeb
Wady <wadyprograming@gmail.com>
2024-12-29 21:49:50 -0600

    Added file and added logic

---
+++ ScoreFormatingColumns.py
@@ -0,0 +1,7 @@
+import os
+os.system('Cls')
+names = ['John', 'Edy', 'Jane','Kane']
+scores = [90,95,80,75]
+print('{0:<10} {1:<5}'.format('Name', 'Score'))
+for index in range(len(names)):
+    print('{0:<10} {1:>5}'.format(names[index], scores[index]))

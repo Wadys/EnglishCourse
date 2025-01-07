aa08f332007c21a8c04a946670235c97fea2565b
Wady <wadyprograming@gmail.com>
2025-01-07 07:01:59 -0600

    Removed extra plus sign in code

--- lists.py
+++ lists.py
@@ -168,7 +168,7 @@ for word in list1:
 print(count)
 
 list1 = [0,1,2,3,4,5,6,7,8,9,10]
-list3 = list[1::2] +
+list3 = list[1::2]
 
 """Create a List from Two Lists:
 Given two lists create a third list by picking an odd-index element from the first list and even-index elements from the second.

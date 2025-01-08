4bbba10ad950a73e19c2192517ce6230c7bf10b1
Wady <wadyprograming@gmail.com>
2025-01-08 09:42:11 -0600

    update

--- lists.py
+++ lists.py
@@ -150,15 +150,13 @@ if 50 in list1:
     list1[list1.index(50)] = 5
 print(list1)
 
-"""
-First and Last Characters
-Implement a function which takes the string type list as a parameter and returns the count of the number of strings where the string length is 2 or more and the first and the last characters are same.
 
-Example:
-list1 = ['cbc', 'xyz', 'aba', '2332', 'abc']
+# First and Last Characters
+# Implement a function which takes the string type list as a parameter and returns the count of the number of strings where the string length is 2 or more and the first and the last characters are same.
 
-Output:
-3"""
+# Example: list1 = ['cbc', 'xyz', 'aba', '2332', 'abc']
+
+# Output: 3
 
 list1 = ['cbc', 'xyz', 'aba', '2332', 'abc']
 count = 0
@@ -170,16 +168,15 @@ print(count)
 list1 = [0,1,2,3,4,5,6,7,8,9,10]
 list3 = list[1::2]
 
-"""Create a List from Two Lists:
-Given two lists create a third list by picking an odd-index element from the first list and even-index elements from the second.
+# Create a List from Two Lists:
+# Given two lists create a third list by picking an odd-index element from the first list and even-index elements from the second.
 
-Input:
-list_one = [4, 12, 16, 21, 24, 28, 32]
-list_two = [5, 10, 15, 20, 25, 30, 35]
+# Input:
+# list_one = [4, 12, 16, 21, 24, 28, 32]
+# list_two = [5, 10, 15, 20, 25, 30, 35]
 
-Output:
-[12, 21, 28, 5, 15, 25, 35]
-"""
+# Output:
+# [12, 21, 28, 5, 15, 25, 35]
 
 list_one = [4, 12, 16, 21, 24, 28, 32]
 list_two = [5, 10, 15, 20, 25, 30, 35]

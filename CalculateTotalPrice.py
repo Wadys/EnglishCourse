c220eb948a97e63fe61048bc959ab2488217dc62
Wady <wadyprograming@gmail.com>
2025-02-04 05:47:00 -0600

    Included optimized solution

--- CalculateTotalPrice.py
+++ CalculateTotalPrice.py
@@ -6,13 +6,13 @@
 # Finally, if it is not out of stock we calculate total price of items and decrease quantity from the stock. This 
 # continues until user select 0. And when the loop is terminated the total price is printed to the console
 from os import system as s
-Inventory = {
-    1: "computer",
-    2: "monitor",
-    3: "keyboard",
-    4: "mouse",
-    5: "hdmi cable",
-    6: "dvd drive"
+inventory = {
+    "1": "computer",
+    "2": "monitor",
+    "3": "keyboard",
+    "4": "mouse",
+    "5": "hdmi cable",
+    "6": "dvd drive"
 }
 price_quantity={
     "computer": {
@@ -43,23 +43,41 @@ price_quantity={
 }
 s('cls')
 total_price = 0
-print("Please add an option from the list")
-for key, value in Inventory.items():
-    print(f"{key}: {value.capitalize()}")
-print("0: to finish")
-option = 1
-while option != 0:
-    option = int(input(""))
-    for key, value in Inventory.items():
-        if option == key:
-            if price_quantity[value]["quantity"] == 0:
-                print(f"{value} is out of stock!")
-            else:
-                total_price += price_quantity[value]["price"]
-                print(f"Adding {value}")
-                price_quantity[value]["quantity"] -= 1 
-        elif option == 0:
-            print(f"Total price: {total_price}")
-            break
+# Original SOlution*******************************************
+# print("Please add an option from the list")
+# for key, value in Inventory.items():
+#     print(f"{key}: {value.capitalize()}")
+# print("0: to finish")
+# option = 1
+# while option != 0:
+#     option = int(input("> "))
+#     for key, value in Inventory.items():
+#         if option == key:
+#             if price_quantity[value]["quantity"] == 0:
+#                 print(f"{value} is out of stock!")
+#             else:
+#                 total_price += price_quantity[value]["price"]
+#                 print(f"Adding {value}")
+#                 price_quantity[value]["quantity"] -= 1 
+#         elif option == 0:
+#             print(f"Total price: {total_price}")
+#             break
     
 
+option = None
+while option != "0":
+    if option in inventory:
+        value = inventory[option]
+        if price_quantity[value]["quantity"] == 0:
+            print(f"{value} is out of stock!")
+        else:
+            total_price += price_quantity[value]["price"]
+            print(f"Adding {value}")
+            price_quantity[value]["quantity"] -= 1
+    else:
+        print("Please add an option from the list")
+        for key, value in inventory.items():
+            print(f"{key}: {value.capitalize()}")
+        print("0: to finish")
+    option = input("> ")
+print(f"Total price: {total_price}")

ad7688927df370079f25263fe3c76fe5d6ca88d9
Wady <wadyprograming@gmail.com>
2025-02-12 07:27:12 -0600

    Updated logic for QuizzApp program

--- QuizzApp.py
+++ QuizzApp.py
@@ -12,44 +12,74 @@
 from AsciiArt import quizapp_logo as logo
 from QuizAppQA import questions_answers as quiz
 from os import system as s
-print(logo)
-print("There are a total of 6 Questions, you can skip a questio any tyme by typing 'skip'")
-input("press any key to get started...")
+
+def greeting(p_question_num):
+    """Print the greeting message with the logo, taking into account the number of questions"""
+    print(logo)
+    print(f"There are a total of {p_question_num} Questions, you can skip a questio any tyme by typing 'skip'")
+    input("press any key to get started...")
+
+def check_answer(question_num, answer, turn, player):
+    """Checks the answer validity and prins if the anwser was correct or wrong."""
+    s("cls")
+    correct_anwswer = quiz[question_num]["answer"]
+    if correct_anwswer.lower() == answer.lower():
+        print(f"Correct Answer! \n{player}'s score is {player_score[player] + 1} ")
+        return True
+    else:
+        print(f"Wrong Answer : ( \n You have {turn -1} attempts left! \nTry Again...)")
+        return False
+
+def switch_users(p_user_index):
+    """Switches players"""
+    if p_user_index == 0:
+        return 1
+    else:
+        return 0
+
+def find_winner(player1, player2):
+    """Determines which player is the winner and prints out if there is a winner or if it's a draw"""
+    if player_score[player1] > player_score[player2]:
+        print(f"{player1} WON! The score is {player_score[player1]}")
+    elif player_score[player1] < player_score[player2]:
+        print(f"{player2} WON! The score is {player_score[player2]}")
+    else:
+        print("IT is a DRAW!")
+
+def print_answers():
+    """Goes throught the questions and answers and prints them to be reviewed by the users"""
+    for question_num, nested_dict in quiz.items():
+        for question, answer in nested_dict.items():
+            print(f"{question} : {answer}")
+
+#MAIN PROGRAM
+greeting(len(quiz))
 players_list = input("Enter 2 players_list separated by spaces: ")
 players_list = players_list.split()
-round = 1
 player_score = dict.fromkeys(players_list,0)
 current_player = players_list[0]
-turns = 2
 for question in quiz:
     print("-"*40)
-    print(f"It is {players_list[current_player]}'s turn")
-    print(quiz[round]["question"])
-    posible_answer = input("Enter Answer (To move to the next question, type 'skip') : ")
-    if posible_answer == quiz[round]["answer"]:
-        player_score[players_list[current_player]] += 1
-        s('cls')
-        print("Correct Answer!")
-        print(f"{players_list[current_player]}'s score is {player_score[players_list[current_player]]}")
-# Switch players
-    else:
-        s("cls")
-        print("Wrong Answer :(")
+    print(f"It is {current_player}'s turn")
+    turns = 2
+    while turns > 0:
+        print(quiz[question]["question"])
+        posible_answer = input("Enter Answer (To move to the next question, type 'skip') : ")
+        if posible_answer == 'skip':
+            break
+        check = check_answer(question, posible_answer, turns, current_player)
+        if check:
+            player_score[current_player] += 1
+            break
         turns -= 1
-        print(f"You have {turns} turns left")
-        if turns != 0:
-            print("Try Again...")
-    if turns == 0 or posible_answer == quiz[round]["answer"]:
-        turns = 2
-        if current_player == 1:
-            current_player = 0
-        else: 
-            current_player = 1
-    round +=1
-print(player_score)
-print_answers = input("Want to see the answers to the questions (Y/N): ")
-if print_answers == 'y':
-    print(quiz)
-print("Thanks for playing")
+    current_player_index = players_list.index(current_player)
+    next_player_index = switch_users(current_player_index)
+    current_player = players_list[next_player_index]
+find_winner(players_list[0], players_list[1])
+show_correct_answer = input("Do you wnat to see correct answers? (Y/N)").lower()
+if show_correct_answer == 'y':
+    print_answers()    
+print("Thanks for playing!")
+
 
 

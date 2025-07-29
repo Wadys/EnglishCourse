a3d9fd1c71acad7212e2542568299d1e9bc6c45e
Wady <wadyprograming@gmail.com>
2025-07-29 12:38:23 -0600

    Created solution for Ancor excercise

---
+++ Excercises/CE88Ancors.py
@@ -0,0 +1,26 @@
+
+#Istructions:
+#Implement a Python function which takes text as a parameter and return the list 
+#of words which are three, four or five characters long.
+
+#Example:
+#find_words("I like Python for Everyone course. It is the best one out there.")
+
+# Output:
+# ['like', 'for', 'the', 'best', 'one', 'out', 'there']
+
+import re
+def find_words(text):
+    regex_text = re.compile(r'\b\w{3,5}\b')
+    text_list = text.split()
+    return_list = []
+    for i in text_list:
+        mo = regex_text.findall(i)
+        return_list.extend(mo)
+    return return_list
+print(find_words("I like Python for Everyone course. It is the best one out there."))
+
+# text ='''I like Python for Everyone course. It is the best one out there.'''
+# regex_text = re.compile(r'\b[a-z]{3,5}\b')
+# mo = regex_text.findall(text)
+# print(mo)
